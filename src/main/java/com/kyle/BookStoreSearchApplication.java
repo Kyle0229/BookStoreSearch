d515ee3a423c22336611bd079ed734d7b4cbbd99
kyle <824238868@qq.com>
2019-11-11 10:10:47 +0800

    Initial commit

---
+++ src/main/java/com/kyle/BookStoreSearchApplication.java
@@ -0,0 +1,13 @@
+package com.kyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BookStoreSearchApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BookStoreSearchApplication.class);
+    }
+}
